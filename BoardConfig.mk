9449eead21e04e0be7085558eca8a65a4aafffdd
DY3413 <milosix3413@gmail.com>
2024-10-01 14:51:52 +0900

    a05s: Include qcom device compatibility matrix
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
